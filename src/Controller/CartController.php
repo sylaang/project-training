9c752e7667b28ad586031664ad45aeb343937cc8
Sylang <sylaang@outlook.fr>
2023-03-29 07:34:13 +0200

    ajout panier

---
+++ src/Controller/CartController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+use App\Service\CartService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+/*
+Clé = Valeur
+[clé] = valeur
+[
+    [clé]=>[valeur]
+]
+/cart/add/7
+/cart/add/8
+/cart/add/8
+    [7]=>1
+    [8]=>2
+*/
+
+#[Route('/cart')]
+class CartController extends AbstractController
+{
+    // page d'ajout de produit
+    #[Route('/add/{id}', name: 'app_cart_add')]
+    public function index
+    ($id,
+    CartService $cartService
+    ):  Response
+    {      
+        $cartService->add($id);
+        // redirection vers la page des produits
+         return $this->redirectToRoute("app_produit_index",[], Response::HTTP_SEE_OTHER);
+
+    }
+
+    // page pour visualiser notre panier
+    #[Route('/show', name: 'app_cart_show')]
+    public function show(
+      CartService $cartService
+        ): Response
+    {
+        
+        return $this->render('cart/index.html.twig', [
+            'panier' => $cartService->show(),
+            
+        ]);
+    }
+
+    // page pour vider notre panier
+    #[Route('/clear', name: 'app_cart_clear')]
+    public function clear(
+        CartService $cartService
+    ): Response
+    {
+        $cartService->clear();
+        
+        // redirection vers la page des produits
+        return $this->redirectToRoute("app_produit_index",[], Response::HTTP_SEE_OTHER);
+
+    }
+}
--- src/Controller/JeuController.php
+++ src/Controller/JeuController.php
@@ -8,35 +8,47 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class JeuController extends AbstractController
 {
     #[Route('/jeu', name: 'app_jeu')]
-    public function index(Request $request, JeuService $jeuService): Response
+    public function index(Request $request, JeuService $jeuService, RequestStack $requestStack): Response
     {
-        $form=$this->createForm(JeuType::class);
+        $form = $this->createForm(JeuType::class);
 
         // on prend l'objet form qui va lire la request
         $form->handleRequest($request);
 
         // test si l'envoie en post et est valide est bien envoyé
         if ($form->isSubmitted() && $form->isValid()) {
- 
+
             // creer une variable data qui est un tableau clé valeur
             // contenant les données envoyé en POST
-            $data= $form->getData();
-    
-           
-    
+            $data = $form->getData();
+
+
+
             // une variable aléatoire va être généré  et 
             // stocké dans le tableau data sur la clé alea
-            $data['alea']=rand(1,100);
-    
-            $data['reponse']=$jeuService->deviner($data['nombre'], $data['alea']);
+            $data['alea'] = rand(1, 100);
+
+            if (
+                (!$requestStack->getSession()->get("nb_alea")) || ($requestStack->getSession()->get("nb_chance")) < 1
+            ) {
+                $data['alea'] = $requestStack->getSession()->set("nb_alea",  $data['alea']);
+                $data['chance'] = $requestStack->getSession()->set("nb_chance", 6);
+            }
+            $requestStack->getSession()->set("nb_chance",  $requestStack->getSession()->get("nb_chance") - 1);
+
+            $data['alea'] = $requestStack->getSession()->get("nb_alea");
+            $data['chance'] = $requestStack->getSession()->get("nb_chance");
+
+            $data['reponse'] = $jeuService->deviner($data['nombre'], $data['alea']);
 
 
-        $this->render('jeu/traitement.html.twig', [
-                'mes_donnes'=>$data,
+            $this->render('jeu/traitement.html.twig', [
+                'mes_donnes' => $data,
             ]);
         }
         return $this->renderForm('jeu/index.html.twig', [
